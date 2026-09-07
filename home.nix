7b5d78a0c1375e22f5aa8847fd3209b3b9d8979b
K3SO <sergulaz05@gmail.com>
2026-09-07 18:41:00 +0200

    Add hyprland config with symlink

--- home.nix
+++ home.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
     home.username = "queso";
     home.homeDirectory = "/home/queso";
     home.stateVersion = "26.05";
+
+    xdg.configFile."hypr" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/config/hypr/";
+        recursive = true;
+    };
 }
