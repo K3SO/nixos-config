b72bee2ab644a7eab245ca6083045cab8479096c
K3SO <sergulaz05@gmail.com>
2026-09-18 18:51:01 +0200

    Update env.lua

--- config/hypr/env.lua
+++ config/hypr/env.lua
@@ -1,8 +1,20 @@
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
+-- Since this hyprland uses UWSM this variables get ignored (i think), but ill leave them just in case.
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Toolkit Backend
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("CLUTTER_BACKEND", "wayland")
+
+-- XDG Specifications
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Qt
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
