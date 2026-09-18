5a928cff948bb5b4c5b182d03c6898e4a6a99c49
K3SO <sergulaz05@gmail.com>
2026-09-18 18:52:17 +0200

    Configure hyprland monitors

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -1,6 +1,15 @@
+-- Main
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
+    output   = "HDMI-A-1",
+    mode     = "1920x1080@143.98Hz",
     scale    = "1",
+    position = "0x0",
+})
+
+-- Built in
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "1920x1080@144.00Hz",
+    scale    = "1",
+    position = "-1920x0",
 })
