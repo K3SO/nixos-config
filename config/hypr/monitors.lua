58df0e8d04bf55edb22f48897c4fbd1dde89786e
K3SO <sergulaz05@gmail.com>
2026-09-20 01:18:33 +0200

    Configure hyprland persistent workspaces

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -13,3 +13,17 @@ hl.monitor({
     scale    = "1",
     position = "-1920x0",
 })
+
+-- Workspaces 1 to 5 on main monitor
+hl.workspace_rule({ workspace = "1", monitor = "HDMI-A-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", persistent = true })
+
+-- Workspaces 6 to 10 on second monitor
+hl.workspace_rule({ workspace = "6", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "7", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "8", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "9", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "10", monitor = "eDP-1", persistent = true })
