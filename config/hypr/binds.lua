b55ae16268e1cb4719659997407eda84b5720c47
K3SO <sergulaz05@gmail.com>
2026-09-20 11:57:02 +0200

    Update hyprland binds and windowrules

--- config/hypr/binds.lua
+++ config/hypr/binds.lua
@@ -1,32 +1,21 @@
----------------------
----- MY PROGRAMS ----
----------------------
-
--- Set programs that you use
+-- Set programs
 local terminal    = "kitty"
 local fileManager = "dolphin"
 local menu        = "hyprlauncher"
 
-
--------------------
----- AUTOSTART ----
--------------------
-
-hl.on("hyprland.start", function () 
+-- Autostart
+hl.on("hyprland.start", function ()
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("discord")
 end)
 
----------------------
----- KEYBINDINGS ----
----------------------
 
-local mainMod = "SUPER" -- Sets "Windows" key as main modifier
+-- Keybinds
+local mainMod = "SUPER"
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
-local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
--- closeWindowBind:set_enabled(false)
+hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
@@ -40,8 +29,6 @@ hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))
 
--- Switch workspaces with mainMod + [0-9]
--- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
     hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
--- config/hypr/windowrules.lua
+++ config/hypr/windowrules.lua
@@ -25,14 +25,12 @@
 
 -- Example window rules that are useful
 
-local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
+hl.window_rule({
     name  = "suppress-maximize-events",
     match = { class = ".*" },
 
     suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
     -- Fix some dragging issues with XWayland
