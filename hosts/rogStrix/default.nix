488f1588d3228c9f722b78b59d7c0ce74e96cfb3
K3SO <sergulaz05@gmail.com>
2026-09-24 10:50:04 +0200

    Move audio section to a module

--- hosts/rogStrix/default.nix
+++ hosts/rogStrix/default.nix
@@ -31,16 +31,6 @@
         withUWSM = true;
     };
 
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-    };
-
     users.users.${username} = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
--- modules/core/default.nix
+++ modules/core/default.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+        ./audio.nix
         ./locale.nix
         ./networking.nix
     ];
